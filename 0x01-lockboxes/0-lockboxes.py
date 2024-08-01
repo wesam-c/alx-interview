ca205903fe25cb097302816ce8d807a60b37a086
wesam-c <wesameldeeb15@gmail.com>
2024-08-02 03:32:11 +0400

    edit

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,26 +1,65 @@
 #!/usr/bin/python3
-"""
-can we open all boxes?
-"""
+"""Solves the lock boxes puzzle """
+
+
+def look_next_opened_box(opened_boxes):
+    """Looks for the next opened box
+    Args:
+        opened_boxes (dict): Dictionary which contains boxes already opened
+    Returns:
+        list: List with the keys contained in the opened box
+    """
+    for index, box in opened_boxes.items():
+        if box.get('status') == 'opened':
+            box['status'] = 'opened/checked'
+            return box.get('keys')
+    return None
 
 
 def canUnlockAll(boxes):
-    """ Return True if all boxes can be opened, else return False """
-    if boxes is None:
-        return False
+    """Check if all boxes can be opened
+    Args:
+        boxes (list): List which contain all the boxes with the keys
+    Returns:
+        bool: True if all boxes can be opened, otherwise, False
+    """
+    if len(boxes) <= 1 or boxes == [[]]:
+        return True
 
-    boxlen = len(boxes)
-    visited = set()
-    stack = [0]
+    aux = {}
+    while True:
+        if len(aux) == 0:
+            aux[0] = {
+                'status': 'opened',
+                'keys': boxes[0],
+            }
+        keys = look_next_opened_box(aux)
+        if keys:
+            for key in keys:
+                try:
+                    if aux.get(key) and aux.get(key).get('status') \
+                       == 'opened/checked':
+                        continue
+                    aux[key] = {
+                        'status': 'opened',
+                        'keys': boxes[key]
+                    }
+                except (KeyError, IndexError):
+                    continue
+        elif 'opened' in [box.get('status') for box in aux.values()]:
+            continue
+        elif len(aux) == len(boxes):
+            break
+        else:
+            return False
 
-    while stack:
-        current_box = stack.pop()
+    return len(aux) == len(boxes)
 
-        if current_box not in visited:
-            visited.add(current_box)
 
-            for key in boxes[current_box]:
-                if key < boxlen and key not in visited:
-                    stack.append(key)
+def main():
+    """Entry point"""
+    canUnlockAll([[]])
 
-    return len(visited) == boxlen
+
+if __name__ == '__main__':
+    main()

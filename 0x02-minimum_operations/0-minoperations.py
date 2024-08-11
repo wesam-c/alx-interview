1a12096bc8ec55c77851444581550d10ceedac1b
wesam-c <wesameldeeb15@gmail.com>
2024-08-11 12:48:27 +0400

    add

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 Module to calculate the fewest number of operations needed to achieve exactly n 'H' characters.
 
@@ -7,13 +6,14 @@ Functions:
     minOperations(n): Returns the minimum number of operations required.
 """
 
+
 def minOperations(n):
     """
     Calculate the fewest number of operations needed to achieve exactly n 'H' characters.
 
     The function uses the factorization of n to determine the minimum number of operations
-    needed to generate exactly n 'H' characters in the text editor. The operations available 
-    are 'Copy All' and 'Paste'. 
+    needed to generate exactly n 'H' characters in the text editor. The operations available
+    are 'Copy All' and 'Paste'.
 
     Parameters:
     n (int): The number of 'H' characters to be achieved.
@@ -29,14 +29,14 @@ def minOperations(n):
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     while n > 1:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
-    
+
     return operations
